acee403ecdcb70a1bbdd1fc026b9265cdeeb1c8e
Oscar Flavio Baggini <flaviobaggini@gmail.com>
2022-09-10 19:19:33 -0300

    Hipopotamo (Oscar Flavio Baggini) flaviobaggini@gmail.com

---
+++ src/clases/Hipopotamo.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class Hipopotamo extends Animal {
+
+	public Hipopotamo(String estructura, String alimentacion, String tipoReproduccion, String habitat,
+			String desplazamiento) {
+		super(estructura, alimentacion, tipoReproduccion, habitat, desplazamiento);
+		// TODO Auto-generated constructor stub
+	}
+
+}
