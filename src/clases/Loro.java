c14608127e3d87db2872ab5314be26de2164d37c
F. Joaquin Fernandez <fjoaquinf@gmail.com>
2022-09-10 05:43:42 -0300

    Loro

---
+++ src/clases/Loro.java
@@ -0,0 +1,19 @@
+package clases;
+
+public class Loro extends Animal {
+
+	public Loro(String estructura, String alimentacion, String tipoReproduccion, String habitat,
+			String desplazamiento) {
+		super(estructura, alimentacion, tipoReproduccion, habitat, desplazamiento);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void Hablarle() {
+		
+	}
+	
+	public void Liberar() {
+		
+	}
+
+}
