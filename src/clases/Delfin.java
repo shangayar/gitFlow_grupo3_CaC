c3224cfb7d79162fde00a7f0290f4677fcf59ba1
DanielDiBartoloit <danieldibartoloit@gmail.com>
2022-09-13 19:12:59 -0300

    Add Delfin class

---
+++ src/clases/Delfin.java
@@ -0,0 +1,7 @@
+package clases;
+
+public class Delfin extends Animal{
+    public Delfin(String estructura, String alimentacion, String tipoReproduccion, String habitat, String desplazamiento) {
+        super(estructura, alimentacion, tipoReproduccion, habitat, desplazamiento);
+    }
+}
